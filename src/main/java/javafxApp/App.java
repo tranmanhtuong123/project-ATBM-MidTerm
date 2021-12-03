82e510559698735c3bab84b5a805127544caa5eb
tranmanhtuong <noobtohero1345@gmail.com>
2021-12-03 15:45:19 +0700

    fix it more, err SplashScreen

--- src/main/java/javafxApp/App.java
+++ src/main/java/javafxApp/App.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafxApp.controller.SplashScreen;
 
 /**
@@ -19,17 +22,18 @@ public class App extends Application {
 
     private static Scene scene;
 
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("main.fxml"), 563, 350);
-        stage.setScene(scene);
-        stage.show();
-    }
+    // public void start(Stage stage) throws IOException {
+    // scene = new Scene(loadFXML("main.fxml"), 563, 350);
+    // stage.setScene(scene);
+    // stage.show();
+    // }
 
     public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml));
         // return fxmlLoader.load();
 
@@ -45,49 +49,39 @@ public class App extends Application {
         launch();
     }
 
-    // private Image splash_image = new Image(
-    //         "https://previews.123rf.com/images/daniilphotos/daniilphotos1905/daniilphotos190500450/122482393-pink-matrix-digital-background-abstract-cyberspace-concept-characters-fall-down-matrix-from-symbols-.jpg",
-    //         563, 350, false, false);
+    private Image splash_image = new Image(
+            "https://previews.123rf.com/images/daniilphotos/daniilphotos1905/daniilphotos190500450/122482393-pink-matrix-digital-background-abstract-cyberspace-concept-characters-fall-down-matrix-from-symbols-.jpg",
+            563, 350, false, false);
 
-    // private Stage mainWindow;
+    private Stage mainWindow;
 
-    // public void start(Stage stage) throws IOException {
-    // new SplashScreen(stage, splash_image, () -> {
+    public void start(Stage stage) throws IOException {
+        new SplashScreen(stage, splash_image, () -> {
 
-    // try {
-    // scene = new Scene(loadFXML("main.fxml"), 563, 350);
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // }
-    // stage.setScene(scene);
-    // stage.show();
+            try {
+                showGame("main.fxml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-    // });
+        });
 
-    // }
+    }
 
-    // private void showGame() throws IOException {
+    private void showGame(String fxml) throws IOException {
 
-    // mainWindow = new Stage();
-    // mainWindow.initStyle(StageStyle.UNDECORATED);
-    // FXMLLoader fxmlLoader = new
-    // FXMLLoader(MainAllToGet.class.getResource("component/view.fxml"));
-    // Parent root = fxmlLoader.load();
+        mainWindow = new Stage();
+        mainWindow.initStyle(StageStyle.UNDECORATED);
+        scene = new Scene(loadFXML("main.fxml"), 563, 350);
 
-    // BorderPane borderPane = new BorderPane();
-    // ToolBar toolBar = new ToolBarHeader(borderPane);
-    // Pane pane = new Header(toolBar, mainWindow);
-    // borderPane.setTop(pane);
-    // borderPane.setCenter(root);
-    // Scene scene = new Scene(borderPane, 600, 590);
-    // final Rectangle2D bounds = Screen.getPrimary().getBounds();
-    // mainWindow.setX(bounds.getMinX() + bounds.getWidth() / 2 - 590 / 2);
-    // mainWindow.setY(bounds.getMinY() + bounds.getHeight() / 2 - 600 / 2);
+        // final Rectangle2D bounds = Screen.getPrimary().getBounds();
+        // mainWindow.setX(bounds.getMinX() + bounds.getWidth() / 2 - 590 / 2);
+        // mainWindow.setY(bounds.getMinY() + bounds.getHeight() / 2 - 600 / 2);
+        // scene.getStylesheets().add(App.class.getResource("Css/mainCss.css").toExternalForm());
 
-    // scene.getStylesheets().add(MainAllToGet.class.getResource("Css/mainCss.css").toExternalForm());
-    // mainWindow.setScene(scene);
-    // mainWindow.show();
-    // root.requestFocus();
-    // }
+        mainWindow.setScene(scene);
+        mainWindow.show();
+
+    }
 
 }
