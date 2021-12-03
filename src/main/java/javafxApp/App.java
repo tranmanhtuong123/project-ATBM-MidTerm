a4b13fbe4706bf884fc473a3f73b6d14c2d041fb
tranmanhtuong <noobtohero1345@gmail.com>
2021-12-03 11:42:55 +0700

    not good

--- src/main/java/javafxApp/App.java
+++ src/main/java/javafxApp/App.java
@@ -6,9 +6,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafxApp.controller.SplashScreen;
 
 /**
  * JavaFX App
@@ -17,11 +19,30 @@ public class App extends Application {
 
     private static Scene scene;
 
+    private Image splash_image = new Image(
+            "https://previews.123rf.com/images/daniilphotos/daniilphotos1905/daniilphotos190500450/122482393-pink-matrix-digital-background-abstract-cyberspace-concept-characters-fall-down-matrix-from-symbols-.jpg",
+            563, 350, false, false);
+
+    // public void start(Stage stage) throws IOException {
+    // scene = new Scene(loadFXML("main.fxml"), 563, 350);
+    // stage.setScene(scene);
+    // stage.show();
+    // }
+
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("main.fxml"), 563, 350);
-        stage.setScene(scene);
-        stage.show();
+        new SplashScreen(stage, splash_image, () -> {
+
+            try {
+                scene = new Scene(loadFXML("main.fxml"), 563, 350);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            stage.setScene(scene);
+            stage.show();
+
+        });
+
     }
 
     public static void setRoot(String fxml) throws IOException {
@@ -43,5 +64,30 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+    // private Stage mainWindow;
+    // private void showGame() throws IOException {
+
+    // mainWindow = new Stage();
+    // mainWindow.initStyle(StageStyle.UNDECORATED);
+    // FXMLLoader fxmlLoader = new
+    // FXMLLoader(MainAllToGet.class.getResource("component/view.fxml"));
+    // Parent root = fxmlLoader.load();
+
+    // BorderPane borderPane = new BorderPane();
+    // ToolBar toolBar = new ToolBarHeader(borderPane);
+    // Pane pane = new Header(toolBar, mainWindow);
+    // borderPane.setTop(pane);
+    // borderPane.setCenter(root);
+    // Scene scene = new Scene(borderPane, 600, 590);
+    // final Rectangle2D bounds = Screen.getPrimary().getBounds();
+    // mainWindow.setX(bounds.getMinX() + bounds.getWidth() / 2 - 590 / 2);
+    // mainWindow.setY(bounds.getMinY() + bounds.getHeight() / 2 - 600 / 2);
+
+    // scene.getStylesheets().add(MainAllToGet.class.getResource("Css/mainCss.css").toExternalForm());
+    // mainWindow.setScene(scene);
+    // mainWindow.show();
+    // root.requestFocus();
+    // }
 
 }
---
+++ src/main/java/javafxApp/controller/SplashScreen.java
@@ -0,0 +1,104 @@
+package javafxApp.controller;
+
+import javafx.animation.FadeTransition;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+/**
+ * This class represents a splash screen which is to be shown while the game is
+ * loading! one the game has loaded the splash screen will fade out!
+ * 
+ * @author Eudy Contreras
+ *
+ */
+public class SplashScreen {
+
+	private FadeTransition fadeSplash;
+
+	private Pane splashLayout;
+
+	private ImageView splash;
+
+	private int splashWidth;
+	private int splashHeight;
+
+	private Image splash_image;
+
+	public SplashScreen(Stage stage, Image splash_Image, InitCompletionHandler showGameMethod) {
+		
+		this.splash_image = splash_Image;
+		 
+
+		this.splash = new ImageView(splash_image);
+		this.splashWidth = (int) splash.getImage().getWidth();
+		this.splashHeight = (int) splash.getImage().getHeight();
+		this.splashLayout = new StackPane();
+		this.splashLayout.getChildren().addAll(splash);
+		this.splashLayout.setBackground(Background.EMPTY);
+		this.splashLayout.setEffect(new DropShadow());
+		this.initSplash(stage, showGameMethod);
+	}
+
+	private void initSplash(Stage primaryStage, InitCompletionHandler showGameMethod) {
+		final Scene splashScene = new Scene(splashLayout, Color.TRANSPARENT);
+		final Rectangle2D bounds = Screen.getPrimary().getBounds();
+		final FadeTransition fade = new FadeTransition(Duration.millis(500), splashLayout);
+		fade.setFromValue(0);
+		fade.setToValue(1);
+		fade.play();
+		fade.setOnFinished(e -> {
+			final Task<Void> task = new Task<Void>() {
+				@Override
+				protected Void call() throws InterruptedException {
+				
+					return null;
+				}
+			};
+			new Thread(task).start();
+			showSplashScreen(primaryStage, task, showGameMethod);
+		});
+		primaryStage.setScene(splashScene);
+		primaryStage.setX(bounds.getMinX() + bounds.getWidth() / 2 - splashWidth / 2);
+		primaryStage.setY(bounds.getMinY() + bounds.getHeight() / 2 - splashHeight / 2);
+		primaryStage.initStyle(StageStyle.TRANSPARENT);
+		primaryStage.setAlwaysOnTop(true);
+		primaryStage.show();
+
+	}
+
+	private void showSplashScreen(final Stage initStage, Task<?> task, InitCompletionHandler initCompletionHandler) {
+		task.stateProperty().addListener((observableValue, oldState, newState) -> {
+			if (newState == Worker.State.SUCCEEDED) {
+				initStage.toFront();
+				FadeTransition fadeSplash = new FadeTransition(Duration.seconds(5), splashLayout);
+				fadeSplash.setFromValue(1.0);
+				fadeSplash.setToValue(0.0);
+				fadeSplash.setOnFinished(actionEvent -> initStage.hide());
+				fadeSplash.play();
+				initCompletionHandler.complete();
+				fadeSplash.setOnFinished(e -> {
+					splashLayout = null;
+					splash = null;
+					initStage.close();
+				});
+			}
+		});
+	}
+
+	public interface InitCompletionHandler {
+		void complete();
+	}
+
+}
